f7f8627da03ff4fb1251b88f6ac6053e2f6c70e8
Joel Lopez Villarreal <joell@southern.edu>
2022-10-23 14:50:18 -0400

    Finished css for video preview.

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,15 +1,31 @@
-const Video = ({ name, isFree, isPurchased, duration, size, price, url }) => {
+const Video = ({ name, isFree, isPurchased, duration, price, url }) => {
+    const blurredVideoCssClass = "video-image-hidden"
+    const visibleVideoCssClass = "video-image"
+
+    // Trim video name to fit inside
+    // video box.
+    if (name.length >= 42) {
+        name = name.substring(0, 42)
+        name += '...'
+    }
+
+    // Remove milliseconds from duration string.
+    duration = duration.match(/[0-9]+:[0-9]+:[0-9]+/g)
+
     return (
         <div className="video-container">
-            <div className="video-image">
-                <video crossOrigin="anonymous" className="video-elem">
+            <div className="video-image-container">
+                <video crossOrigin="anonymous" className={(isFree || isPurchased ? visibleVideoCssClass : blurredVideoCssClass)}>
                     <source src={url} type="video/mp4" />
                 </video>
+                {(!isFree && !isPurchased) && <p className="paid-video-symbol">$</p>}
                 <p className="video-duration">{duration}</p>
             </div>
             <div className="video-information-container">
                 <p className="video-title">{name}</p>
-                <p className="favorites-icon">♡</p>
+                {(!isFree && !isPurchased) && (<button className="add-to-cart-button"><i className="bi bi-cart-plus"></i></button>)}
+                {(!isFree && !isPurchased) && (<p className="video-price-label">${price}</p>)}
+                {isFree && <p className="favorites-icon"><i className="bi bi-suit-heart favorites-icon"></i></p>}
             </div>
         </div>
     )
